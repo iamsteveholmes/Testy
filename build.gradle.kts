081dd720fde4ecf896484f10e0fb07340686ca88
Steve <iamsteveholmes@gmail.com>
2021-08-11 09:43:06 -0700

    updated to kotlin 1.5.30-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
-        classpath("com.android.tools.build:gradle:7.1.0-alpha07")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.30-RC")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha08")
         classpath("com.squareup.sqldelight:gradle-plugin:$sql_delight_version")
         classpath("io.realm.kotlin:gradle-plugin:$realm_version")
     }
